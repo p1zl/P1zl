f96f96242c304d1c56de8d3b8995e163d0bb9555
p1zl <mikmirn@gmail.com>
2024-07-12 23:08:30 -0700

    project.py
    I am not done with this project but this my first try

---
+++ learn.py
@@ -0,0 +1,12 @@
+student1 = "I am in school"
+student2 = "I am in course"
+settings_school = ["Math", "PE", "Social Science", "Science", "English"]
+settings_course = ["Algebra", "Geography", "Programming"]
+if student1 is "I am in course":
+    print (settings_course)
+else: 
+    print (settings_school)
+if student2 is "I am in course":
+    print (settings_course)
+else:
+    print (settings_school)
